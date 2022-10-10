ee949ff0c2f269b0dae43e43ec4fab7d7dad7fcc
Crystal-dart230 <113368574+Crystal-dart230@users.noreply.github.com>
2022-10-10 11:06:28 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+let y=0;
+
+function setup() {
+  createCanvas(800,800);
+}
+
+function draw() {
+  background(13,12,12);
+  
+fill(207,213,234);
+  rect(y+500,300,200);
+  circle(y+600, 200, 200);
+  ellipse(y+620, 250, 45, 45);
+fill(207,213,234); 
+  
+  strokeWeight(10);
+  line(y+500, 400, 1000, 105);
+  line(y+700, 400, 900, 180);
+ fill(207,213,234); 
+  
+  
+  circle(y+650, 200,30);
+  circle(y+580, 200,30);
+  stroke('#3F51B5');
+ 
+  size=100;
+  
+  
+fill(251,236,93);
+  fill(random(225,0));
+arc(y+200, 390, 300, 300, radians(30), radians(330), PIE);
+y= y+1 
+fill(251,236,93);  
+circle(y+200,300,30);
+circle(y+100,50,30);
+circle(y+200,50,30);
+circle(y+300,50,30);
+circle(y+400,50,30); 
+circle(y+500,50,30); 
+circle(y+600,50,30);
+circle(y+700,50,30); 
+circle(y+100,100,30);  
+circle(y+200,100,30); 
+circle(y+300,100,30);  
+circle(y+400,100,30); 
+circle(y+500,100,30);  
+circle(y+700,100,30);  
+circle(y+100,150,30);  
+circle(y+200,150,30);  
+circle(y+300,150,30);  
+circle(y+400,150,30);  
+circle(y+100,200,30);  
+circle(y+200,200,30);  
+circle(y+300,200,30);  
+circle(y+400,200,30);  
+circle(y+100,250,30);  
+circle(y+300,250,30);  
+circle(y+400,250,30);  
+circle(y+490,250,30);  
+circle(y+710,250,30);  
+circle(y+50,500,30);
+circle(y+400,500,30);
+circle(y+480,500,30);  
+circle(y+730,500,30);  
+circle(y+50,600,30);  
+circle(y+50,500,30);  
+circle(y+50,500,30);  
+circle(y+50,500,30);  
+circle(y+50,500,30);  
+circle(y+50,500,30); 
+circle(y+50,500,30);  
+circle(y+50,500,30); 
+}
